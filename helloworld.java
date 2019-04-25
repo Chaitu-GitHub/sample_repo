2b7572c77a91145d24e3021eb599874f4fc717b8
Chaitu <chaituchaitu9949@gmail.com>
2019-04-25 16:03:48 +0000

    commit 2:updated new line

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,12 @@
 
 public class HelloWorld
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+        
+         {
 		System.out.println("Hello World!");
 	}
+        {
+                System.out.println("welcome to Devops");
+        }
 }
